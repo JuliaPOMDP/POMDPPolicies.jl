bd5e695d174f5b3119085d2278977853866e9b48
Maxime <MaximeBouton@users.noreply.github.com>
2018-10-25 10:11:32 -0700

    update valuepolicy docs

--- src/vector.jl
+++ src/vector.jl
@@ -35,6 +35,8 @@ end
 """
      ValuePolicy{P<:Union{POMDP,MDP}, T<:AbstractMatrix{Float64}, A}
 A generic MDP policy that consists of a value table. The entry at `stateindex(mdp, s)` is the action that will be taken in state `s`.
+It is expected that the order of the actions in the value table is consistent with the order of the actions in `act`. 
+If `act` is not explicitly set in the construction, `act` is ordered according to `actionindex`.
 
 # Fields 
 - `mdp::P` the MDP problem
