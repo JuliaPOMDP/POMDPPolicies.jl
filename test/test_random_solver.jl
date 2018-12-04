7e852e586c6b1723bdd6ab36dab8c4dded3907e2
MaximeBouton <maxim.bouton@gmail.com>
2018-12-04 11:50:00 -0800

    fix warning

--- test/test_random_solver.jl
+++ test/test_random_solver.jl
@@ -7,7 +7,7 @@ let
 
     sim = RolloutSimulator(max_steps=10, rng=MersenneTwister(1))
 
-    r = simulate(sim, problem, policy, updater(policy), initial_state_distribution(problem))
+    r = simulate(sim, problem, policy, updater(policy), initialstate_distribution(problem))
 
     @test isapprox(r, -27.27829, atol=1e-3)
 end
