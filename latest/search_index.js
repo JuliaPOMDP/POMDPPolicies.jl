9e90f0b23ccc149a92fb3552d6cf288e26fc70a6
autodocs <autodocs>
2018-12-07 00:07:25 +0000

    build based on 006f558

--- latest/search_index.js
+++ latest/search_index.js
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Alpha Vector Policy",
     "title": "Alpha Vector Policy",
     "category": "section",
-    "text": "Represents a policy with a set of alpha vectors. Can be constructed with AlphaVectorPolicy(pomdp, alphas, actionmap), where alphas is either a vector of vectors or an |S| x |A| matrix. The actionmap argument is a vector of actions with length equal to the number of alpha vectors. If this argument is not provided, ordered_actions is used to generate a default action map. function.jlAlphaVectorPolicy"
+    "text": "Represents a policy with a set of alpha vectors. Can be constructed with AlphaVectorPolicy(pomdp, alphas, action_map), where alphas is either a vector of vectors or an |S| x |A| matrix. The action_map argument is a vector of actions with length equal to the number of alpha vectors. If this argument is not provided, ordered_actions is used to generate a default action map. function.jlAlphaVectorPolicy"
 },
 
 {
